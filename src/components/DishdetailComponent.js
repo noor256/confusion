87a38c735147cfc079ee7f4c4d19f6cbc713c46f
ibrahima <lodiajames@hotmail.com>
2022-06-20 13:32:45 -0400

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardBody, CardTitle, CardText, CardImgOverlay,Breadcrumb, BreadcrumbItem, Button, Modal, ModalBody, ModalHeader, Label, Row, Col} from 'reactstrap';
 import { DISHES } from '../shared/dishes';
+import { Loading } from './LoadingComponent';
 import { Link } from 'react-router-dom';
 
 
@@ -143,8 +144,27 @@ class CommentForm extends Component {
 
 
    function RenderDish({dish}) {
+           
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
 
-        if (dish != null) {
+       else if (props.dish != null) {
             return (
                 <div className='col-12 col-md-5 m-1'>
                     <Card>
